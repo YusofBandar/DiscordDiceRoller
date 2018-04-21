862e78cfa3096a82c2024fd21a741d8d5002db68
Yusof Bandar <ybandar1902@gmail.com>
2018-04-21 13:30:05 +0100

    Life Keeper

---
+++ commands/LifeTotal.js
@@ -0,0 +1,40 @@
+module.exports = {
+    name: 'Life Keeper',
+    command: '!rolllife',
+    description: 'Maintains Life',
+    example: '!rolllife set20 => member life to 20',
+    execute(client, message, args) {
+
+        var command = message.content.split(" ")
+        //set author life
+        if (command[1].startsWith("set")) {
+            var life = lifeExtractor(command[1]);
+            console.log(life[0]);
+    
+        }
+        //add to author life 
+        else if (command[1].startsWith("add")) {
+        
+        }
+        //minus from authors life 
+        else if (command[1].startsWith("minus")) {
+
+        }
+
+        message.reply(" life is ");
+
+        function lifeExtractor(message) {
+            var regex = /\d{1,3}/g;
+            var life = [];
+            life = regex.exec(message);
+
+            if (life == null) {
+                life = [];
+                life.push(20);
+            }
+
+            return life;
+
+        }
+    }
+}
