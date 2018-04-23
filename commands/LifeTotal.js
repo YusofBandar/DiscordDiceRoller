405b5be72b1f7d9708e136e889ba9c0331acf6fc
unknown <ybandar1902@gmail.com>
2018-04-23 23:20:53 +0100

    newGame

--- commands/LifeTotal.js
+++ commands/LifeTotal.js
@@ -7,24 +7,49 @@ module.exports = {
 
         var gameObj = function(gameName){
             this.name = gameName;
+            this.members = [];
+        }
+
+        var memberObj = function(memberName,memberLife,admin){
+            this.memberName = memberName;
+            this.memberLife = memberLife;
+            this.admin = admin;
         }
 
         var currentGames = [];
         var command = message.content.split(" ")
         //start new game
         if (command[1].startsWith("newGame")) {
-            var game = new gameObj("test");
+            var gameName;
+            if(command[2] === undefined){
+                gameName = "Game"
+            }else{
+                gameName = command[2];
+            }
+            var game = new gameObj(gameName);
+            var member = new memberObj(message.author.username,20,true);
+            game.members.push(member);
+            currentGames.push(game);
+
+            message.reply(command[2] + " game added");
         }
         //add to author life 
-        else if (command[1].startsWith("add")) {
-        
+        else if (command[1].startsWith("addmember")) {
+            var mentions = Array.from(message.mentions.users);
+            var index = -1;
+
+            console.log(currentGames);
+
+            mentions.forEach(user => {
+                //console.log(user[1].username);
+            });
         }
         //minus from authors life 
         else if (command[1].startsWith("minus")) {
 
         }
 
-        message.reply(" life is ");
+        
 
         function lifeExtractor(message) {
             var regex = /\d{1,3}/g;
